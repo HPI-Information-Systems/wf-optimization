7627eaae9a2079758c2f1f3d4df016b189efe2db
dey4ss <27929897+dey4ss@users.noreply.github.com>
2026-05-19 17:36:27 +0200

    adapt equivalence

--- evaluation/resources/equiv13_after.sql
+++ evaluation/resources/equiv13_after.sql
@@ -2,5 +2,5 @@
   SELECT *
     FROM (SELECT *,
   	             rank() OVER (order by price, sale_id) res FROM sales) AS s
--- ORDER BY price
+ORDER BY price
    LIMIT 20;
--- evaluation/resources/equiv13_before.sql
+++ evaluation/resources/equiv13_before.sql
@@ -2,4 +2,4 @@ SELECT *
   FROM (SELECT *,
   	           rank() OVER (order by price, sale_id) res
   	      FROM sales) AS s
- WHERE res <= 20;
+ WHERE res <= 3;
