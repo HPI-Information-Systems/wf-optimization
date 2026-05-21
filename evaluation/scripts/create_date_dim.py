4e1e878558c71fe92649e6c5f04018c746d9add1
dey4ss <27929897+dey4ss@users.noreply.github.com>
2026-05-21 16:25:06 +0200

    fix for early years

--- evaluation/scripts/create_date_dim.py
+++ evaluation/scripts/create_date_dim.py
@@ -22,10 +22,17 @@ def main(sales_path, output_dir):
     min_year = min(sales.sold_date)[:4]
     max_year = max(sales.sold_date)[:4]
 
+    min_date = f"{min_year}-01-01"
+    max_date = f"{max_year}-12-31"
+
+    min_ordinal = datetime.date.fromisoformat(min_date).toordinal()
+    max_ordinal = datetime.date.fromisoformat(max_date).toordinal()
+
     with open(os.path.join(output_dir, f"date_dim{sales_file_name[len('sales'):]}"), "w") as f:
         f.write("d_date_sk,d_date,d_year,d_moy,d_dom\n")
-        for date in pd.date_range(start=f"{min_year}-01-01", end=f"{max_year}-12-31"):
-            f.write(f"{date.date().toordinal()},{date.date().isoformat()},{date.year},{date.month},{date.day}\n")
+        for date_ordinal in range(min_ordinal, max_ordinal + 1):
+            date = datetime.date.fromordinal(date_ordinal)
+            f.write(f"{date_ordinal},{date.isoformat()},{date.year},{date.month},{date.day}\n")
 
 
 if __name__ == '__main__':
